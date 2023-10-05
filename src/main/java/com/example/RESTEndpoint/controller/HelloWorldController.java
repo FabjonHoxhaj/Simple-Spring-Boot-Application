f841ffb19b906c52a8689eebe0fe15fde60e6cd5
unknown <fahox1991@gmail.com>
2023-10-05 13:27:54 +0200

    local date

--- src/main/java/com/example/RESTEndpoint/controller/HelloWorldController.java
+++ src/main/java/com/example/RESTEndpoint/controller/HelloWorldController.java
@@ -2,12 +2,17 @@ package com.example.RESTEndpoint.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
 
 @RestController
 public class HelloWorldController {
 
+LocalDate localDate = LocalDate.now();
+
 @GetMapping("/hallo")
-public String helloWorld() {
-return "Hallo Liebe coole Welt";
+public LocalDate helloWorld() {
+return this.localDate;
 }
+
+
 }
